f96cdd7cf51c39b4b21c5c3592a9aecfeb0498bd
Lennart Kester <lennartkester@gmail.com>
2020-05-29 16:13:56 +0200

    inital commit

---
+++ runArrayApp.R
@@ -0,0 +1,6 @@
+## Run UMCU array tools App ##
+
+## to open the app click Run App button in top right corner of this window ##
+setwd("/Users/lennartkester/Documents/EPIC/UMC_arrayTools/")
+library(shiny)
+runApp("NGSapp")
