b1303f3667cef8b73b381defab05b2598af1dc9c
Lennart Kester <lennartkester@gmail.com>
2020-06-05 17:00:31 +0200

    bug fixes

--- runArrayApp.R
+++ runArrayApp.R
@@ -1,6 +1,6 @@
 ## Run UMCU array tools App ##
 
 ## to open the app click Run App button in top right corner of this window ##
-setwd("/Users/lennartkester/Documents/EPIC/UMC_arrayTools/")
+setwd("T:/pathologie/KMBP/EPIC/UMC_arrayTools/")
 library(shiny)
-runApp("NGSapp")
+runApp("arrayApp")
